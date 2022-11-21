2381a30a8b56531ae8ea3be21960e09cf377e22d
gesnimbar <engura.izi@gmail.com>
2022-11-21 13:24:47 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   LINK: 'https://gesnimbar.studio.site/', // 网站地址
   KEYWORDS: 'Notion, 博客, Blog', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
-  process.env.NOTION_PAGE_ID || 'xxxx', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  process.env.NOTION_PAGE_ID || '5216a934206a4764a211991a9ef977a0', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
