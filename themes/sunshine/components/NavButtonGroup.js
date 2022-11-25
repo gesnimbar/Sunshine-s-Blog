f1f540d83712ba472976acf859404e7367e1bc68
gesnimbar <engura.izi@gmail.com>
2022-11-25 17:34:09 +0900

    Change Button and Notion caption

--- themes/sunshine/components/NavButtonGroup.js
+++ themes/sunshine/components/NavButtonGroup.js
@@ -10,10 +10,10 @@ import Link from 'next/link'
 const NavButtonGroup = (props) => {
   const { categories } = props
 
-  return <nav id='home-nav-button' className={'md:h-40 md:mt-6 xl:mt-24 px-5 py-2 mt-8 flex flex-wrap md:max-w-5xl space-y-2 md:space-y-0 md:flex justify-center max-h-40 overflow-auto'}>
+  return <nav id='home-nav-button' className={'h-40 md:h-40 md:mt-6 xl:mt-6 px-5 py-2 mt-6 flex flex-wrap max-w-7xl space-y-2 md:space-y-0 md:flex justify-center max-h-40 overflow-auto'}>
     {categories.map(category => {
       return <Link key={`${category.name}`} title={`${category.name}`} href={`/category/${category.name}`} passHref>
-            <a className='text-center w-full md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism  hover:bg-white hover:text-black duration-200 font-bold hover:scale-105 transform'>{category.name}</a>
+            <a className='text-center w-full md:mx-6 md:w-40 h-14 justify-center items-center flex border-0 cursor-pointer rounded-md border-2 border-gray-300 glassmorphism drop-shadow-[0_35px_35px_rgba(1,1,1,1)]  hover:bg-white hover:text-black duration-200 font-bold hover:scale-105 transform'>{category.name}</a>
         </Link>
     })}
   </nav>
